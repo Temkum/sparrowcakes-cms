59fcdade3cc19206726a5809eedb680a68a3ded0
Kum Jude Tem <judekum14@gmail.com>
2025-02-01 23:29:43 +0100

    add utility function for class name merging and update App component styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="text-red-300 text-3xl">hello</h1>
+      <h1 className="text-red-800 text-3xl">hello world</h1>
       <a href="" className="text-pretty text-2xl text-blue-300">
         Hover
       </a>
