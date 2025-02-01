2b1e4efe720360c74f03c99c73be746f92932147
Kum Jude Tem <judekum14@gmail.com>
2025-02-01 21:43:23 +0100

    initialize project with Vite, React, and TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-300 text-3xl">hello</h1>
+      <a href="" className="text-pretty text-2xl text-blue-300">
+        Hover
+      </a>
+    </>
+  );
+}
+
+export default App;
